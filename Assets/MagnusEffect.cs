ca09a97fdf668d526f1284588d2fe9e35a165c7a
Koalku <phuriwat6585@gmail.com>
2025-03-20 21:34:44 +0700

    เสร็จละ

---
+++ Assets/MagnusEffect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MagnusEffect : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // kick
+            Kick();
+        }
+
+        // apply magnus effect
+        ApplyMagnusEffect();
+    }
+
+    void Kick()
+    {
+        // แรงที่พุ่งตรงไปหา Goal
+        rb.linearVelocity = velocity;
+
+        // แรงหมุนบอล
+        rb.angularVelocity = spin;
+    }
+
+    void ApplyMagnusEffect()
+    {
+        Vector3 magnusForce = Vector3.Cross(rb.linearVelocity, rb.angularVelocity);
+        rb.AddForce(magnusForce);
+    }
+}
